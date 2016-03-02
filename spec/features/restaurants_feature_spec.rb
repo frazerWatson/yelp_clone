5616e390679686b169b83b011623aaf0c21c72be
Douglas Rose <douglahr@gmail.com>
2016-03-02 12:54:42 +0100

    added 2nd feature test, switching screens due to technical difficulties

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -6,7 +6,19 @@ feature 'restaurants' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
-      
+
+    end
+
+  end
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restuarants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
     end
 
   end
