b035ad0049f395cfc7d0f0f392ba5f3f7de85098
Frazer Watson <frazerw@tutanota.com>
2016-03-03 18:03:53 +0000

    Omniauth.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,6 +249,10 @@ Devise.setup do |config|
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
 
+  config.omniauth Rails.application.secrets.[facebook_app_id:]
+     Rails.application.secrets.[facebook_app_secret:]
+    callback_url: "http://localhost:3000"
+
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
